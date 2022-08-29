ddbcd6d0fb01b75de8c1b7eb333ab1a72333d522
danyaITi <danil.lenskiy.1997@mail.ru>
2022-08-29 20:30:29 +0900

    add hashTable

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { store } from './redux/store'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
 
